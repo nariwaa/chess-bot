92401dbc3ce7af57fa7bb71b58305e1e3afb55a2
nariwaa <lucasval3n2c@gmail.com>
2025-04-15 13:47:51 +0200

    got a random chess bot

--- experimenting.py
+++ experimenting.py
@@ -1,12 +1,36 @@
-# %%
-print("hi")
-
-# %%
+# %% reset board
 import torch
+import torch.nn as nn
+import random
 import chess
+from time import sleep
 board = chess.Board()
 # %%
 print(board)
+# %% random moves lmao
+def randmv(time):
+    n = 0
+    board = chess.Board()
+    while not board.is_game_over():
+        n+=1
+        print(f"{board}\n n = {n} \n")
+        sleep(time)
+        legal = list(board.legal_moves)
+        move = random.choice(legal)
+        board.push(move)
+    return(int(n))
 # %%
-print(board.legal_moves)
+print(randmv(0))
 
+# %% avg mv
+def avg_n_randmv(count: int):
+    countrn = 0
+    ntotal=0
+    while countrn < count:
+        countrn+=1
+        n = randmv(0)
+        ntotal = ntotal+n
+    nfinal = ntotal / count
+    return(nfinal)
+
+avg_n_randmv(100)
